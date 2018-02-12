e5b7e4ded89736a66b99313fadd47d5c076cb53b
Will Davis <twd38@cornell.edu>
2018-02-12 17:12:04 -0500

    meme test2

--- pictureServer.js
+++ pictureServer.js
@@ -168,8 +168,13 @@ io.on('connect', function(socket) {
     caption.path(originalFile, {
       caption: 'Meme it.',
       outputFile: saveToFile},
-      fuction(err,filename){
-        io.emit('newPicture',imageName+'_resized.jpg');
+      fuction(err){
+        if (!err) {
+              console.log('memed');
+              io.emit('newPicture',imageName+'_resized.jpg');
+            } else {
+              console.log(err)
+            }
       });
 
 
