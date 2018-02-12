c09ce8b6ffeba8e89c576981de4975afc7966a53
Will Davis <twd38@cornell.edu>
2018-02-12 15:41:49 -0500

    meme test2

--- pictureServer.js
+++ pictureServer.js
@@ -26,6 +26,7 @@ var NodeWebcam = require( "node-webcam" );// load the webcam module
 var fs = require('fs')
   , gm = require('gm');
 let memeMaker = require('meme-maker');
+var imageName = '';
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
@@ -122,7 +123,7 @@ io.on('connect', function(socket) {
     /// First, we create a name for the new picture.
     /// The .replace() function removes all special characters from the date.
     /// This way we can use it as the filename.
-    var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+    imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
 
 
     console.log('making a making a picture at '+ imageName); // Second, the name is logged to the console.
